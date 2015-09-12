b44f631e07e7084fe2bc918c7e0d4928c856d1d0
binaryk <lupacescueduard@yahoo.com>
2015-09-12 12:41:09 +0300

    Curent work

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,5 @@ Route::get('url-get-airports',['as' => 'r_get_airports', 'uses' => 'HomeControll
 include(app_path() . '/~Libs/routes/routes_booking_form.php');
 
 // un mesaj frumos
+// inca un mesaj
+// 
