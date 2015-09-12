964685ce1456ac94fc4c44fe0ad84909bc2cac94
binaryk <lupacescueduard@yahoo.com>
2015-09-12 14:59:59 +0300

    Curent work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,9 +13,9 @@ class HomeController extends Controller {
 
 	public function index()
 	{
-		dd('salut');
-		dd('salut');
 		dd(basename(__FILE__));
+
+		// test
 
 		$controls = $this->controls('up_');
 		$controls1 = $this->controls('off_');
