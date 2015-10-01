71f12ec5cc4bdb314394e692b6e65cafbc1d64cd
binaryk <lupacescueduard@yahoo.com>
2015-10-01 11:42:10 +0300

    Curent work

--- app/Http/Middleware/iblink_final/app/Models/Libs/Clasaelev.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App\Models\Libs;
-
-class Clasaelev extends \Illuminate\Database\Eloquent\Model 
-{
-
-    protected $table = 'group_student';
-
-
-}
--- app/Http/Middleware/iblink_final/app/~Libs/~system/form/controls/font.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Easy\Form;
-
-class Font
-{
-
-}
